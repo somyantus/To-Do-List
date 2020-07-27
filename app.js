c821b9c456f2e95258041f7b14b27efa05e6640d
Somya <somyaagarwal2000@gmail.com>
2020-07-27 15:40:09 +0530

    Add gitignore and procfile

--- app.js
+++ app.js
@@ -140,6 +140,12 @@ app.get("/about", function (req, res) {
     res.render("about");
 })
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function () {
     console.log("hello world");
 });
